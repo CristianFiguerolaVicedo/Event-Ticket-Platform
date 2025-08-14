4795176fdf60ece77a8e9bbc29d4ca1ea84480d6
CriFigShe <crifigshe@gmail.com>
2025-08-14 12:54:40 +0200

    Project imported from spring initializer

---
+++ src/test/java/com/cristian/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
