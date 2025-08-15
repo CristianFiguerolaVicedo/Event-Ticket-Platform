a5bae6f9003b8a1692683050fbb89f6dbe6f7356
CriFigShe <crifigshe@gmail.com>
2025-08-15 11:23:40 +0200

    User Provisioning filter

---
+++ src/main/java/com/cristian/tickets/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cristian.tickets.repositories;
+
+import com.cristian.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
