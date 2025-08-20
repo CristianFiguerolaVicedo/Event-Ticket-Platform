666d9c9c9ab9c91653530d2ec5ebed661e0ff969
CriFigShe <crifigshe@gmail.com>
2025-08-20 11:57:19 +0200

    List events mapper and dto

--- src/main/java/com/cristian/tickets/mappers/EventMapper.java
+++ src/main/java/com/cristian/tickets/mappers/EventMapper.java
@@ -2,10 +2,9 @@ package com.cristian.tickets.mappers;
 
 import com.cristian.tickets.domain.CreateEventRequest;
 import com.cristian.tickets.domain.CreateTicketTypeRequest;
-import com.cristian.tickets.domain.dtos.CreateEventRequestDto;
-import com.cristian.tickets.domain.dtos.CreateEventResponseDto;
-import com.cristian.tickets.domain.dtos.CreateTicketTypeRequestDto;
+import com.cristian.tickets.domain.dtos.*;
 import com.cristian.tickets.domain.entities.Event;
+import com.cristian.tickets.domain.entities.TicketType;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -14,4 +13,6 @@ public interface EventMapper {
     CreateTicketTypeRequest fromDto(CreateTicketTypeRequestDto dto);
     CreateEventRequest fromDto(CreateEventRequestDto dto);
     CreateEventResponseDto toDto(Event event);
+    ListEventTicketTypeResponseDto toDto(TicketType ticket);
+    ListEventResponseDto toListEventResponseDto(Event event);
 }
