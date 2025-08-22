942de66e8149de94aefc37409a89050952519833
CriFigShe <crifigshe@gmail.com>
2025-08-22 12:42:42 +0200

    get published event service

--- src/main/java/com/cristian/tickets/services/impl/EventServiceImpl.java
+++ src/main/java/com/cristian/tickets/services/impl/EventServiceImpl.java
@@ -149,6 +149,11 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Page<Event> searchPublishedEvents(String query, Pageable pageable) {
+        return eventRepository.searchEvents(query, pageable);
+    }
 
+    @Override
+    public Optional<Event> getPublishedEvent(UUID id) {
+        return eventRepository.findByIdAndStatus(id, EventStatusEnum.PUBLISHED);
     }
 }
