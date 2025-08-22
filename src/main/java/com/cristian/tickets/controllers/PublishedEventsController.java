f7777dfc028496123388d418758b2b9dff206e6a
CriFigShe <crifigshe@gmail.com>
2025-08-22 12:09:06 +0200

    List published events controller and security config

---
+++ src/main/java/com/cristian/tickets/controllers/PublishedEventsController.java
@@ -0,0 +1,27 @@
+package com.cristian.tickets.controllers;
+
+import com.cristian.tickets.domain.dtos.ListPublishedEventsResponseDto;
+import com.cristian.tickets.mappers.EventMapper;
+import com.cristian.tickets.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/published-events")
+@RequiredArgsConstructor
+public class PublishedEventsController {
+
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @GetMapping
+    public ResponseEntity<Page<ListPublishedEventsResponseDto>> listPublishedEvents(Pageable pageable) {
+        return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
+                .map(eventMapper::toListPublishedEventsResponseDto));
+    }
+}
