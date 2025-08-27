ab661ff5e02b40cec693d388b36d147ff94f6ac1
CriFigShe <crifigshe@gmail.com>
2025-08-27 10:16:08 +0200

    List ticket service and implementation

---
+++ src/main/java/com/cristian/tickets/services/TicketService.java
@@ -0,0 +1,11 @@
+package com.cristian.tickets.services;
+
+import com.cristian.tickets.domain.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface TicketService {
+    Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable);
+}
---
+++ src/main/java/com/cristian/tickets/services/impl/TicketServiceImpl.java
@@ -0,0 +1,23 @@
+package com.cristian.tickets.services.impl;
+
+import com.cristian.tickets.domain.entities.Ticket;
+import com.cristian.tickets.repositories.TicketRepository;
+import com.cristian.tickets.services.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TicketServiceImpl implements TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    @Override
+    public Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable) {
+        return ticketRepository.findByPurchaserId(userId, pageable);
+    }
+}
