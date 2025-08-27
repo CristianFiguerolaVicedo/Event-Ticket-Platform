e4ef3c1005d0a9be8a81e6f2a706c7611bbb3c0e
CriFigShe <crifigshe@gmail.com>
2025-08-27 10:56:44 +0200

    Qr code return service and implementation

--- src/main/java/com/cristian/tickets/services/QrCodeService.java
+++ src/main/java/com/cristian/tickets/services/QrCodeService.java
@@ -3,6 +3,9 @@ package com.cristian.tickets.services;
 import com.cristian.tickets.domain.entities.QrCode;
 import com.cristian.tickets.domain.entities.Ticket;
 
+import java.util.UUID;
+
 public interface QrCodeService {
     QrCode generateQrCode(Ticket ticket);
+    byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }
--- src/main/java/com/cristian/tickets/services/impl/QrCodeServiceImpl.java
+++ src/main/java/com/cristian/tickets/services/impl/QrCodeServiceImpl.java
@@ -4,6 +4,7 @@ import com.cristian.tickets.domain.entities.QrCode;
 import com.cristian.tickets.domain.entities.QrCodeStatusEnum;
 import com.cristian.tickets.domain.entities.Ticket;
 import com.cristian.tickets.exceptions.QrCodeGenerationException;
+import com.cristian.tickets.exceptions.QrCodeNotFoundException;
 import com.cristian.tickets.repositories.QrCodeRepository;
 import com.cristian.tickets.services.QrCodeService;
 import com.google.zxing.BarcodeFormat;
@@ -12,6 +13,7 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QrCodeServiceImpl implements QrCodeService {
 
     private static final int QR_HEIGHT = 300;
@@ -48,6 +51,19 @@ public class QrCodeServiceImpl implements QrCodeService {
 
         } catch (WriterException | IOException ex) {
             throw new QrCodeGenerationException("Failed to generate the QR code", ex);
+        }
+    }
+
+    @Override
+    public byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId) {
+        QrCode qrCode = qrCodeRepository.findByTicketIdAndTicketPurchaserId(ticketId, userId)
+                .orElseThrow(QrCodeNotFoundException::new);
+
+        try {
+            return Base64.getDecoder().decode(qrCode.getValue());
+        } catch (IllegalArgumentException ex) {
+            log.error("Invalid base 64 qr code for ticket ID: {}", ticketId, ex);
+            throw new QrCodeNotFoundException();
         }
     }
 
