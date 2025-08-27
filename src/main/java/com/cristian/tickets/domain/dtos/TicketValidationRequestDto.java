3135cba998e9e9a9e219cc366b636a69f1b11980
CriFigShe <crifigshe@gmail.com>
2025-08-27 11:47:48 +0200

    Ticket validation dto and mapper

---
+++ src/main/java/com/cristian/tickets/domain/dtos/TicketValidationRequestDto.java
@@ -0,0 +1,17 @@
+package com.cristian.tickets.domain.dtos;
+
+import com.cristian.tickets.domain.entities.TicketValidationMethod;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketValidationRequestDto {
+
+    private UUID id;
+    private TicketValidationMethod method;
+}
---
+++ src/main/java/com/cristian/tickets/domain/dtos/TicketValidationResponseDto.java
@@ -0,0 +1,17 @@
+package com.cristian.tickets.domain.dtos;
+
+import com.cristian.tickets.domain.entities.TicketValidationStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketValidationResponseDto {
+
+    private UUID ticketId;
+    private TicketValidationStatusEnum status;
+}
